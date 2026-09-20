30aca15774a3dd77166d11fac1a15e9ee8b13dba
osama abu sharekh <abosharekhosama@gmail.com>
2026-09-20 23:47:53 +0300

    fix: resolve nest main bodyparser and office conversion typescript errors 4

--- backend/src/office/office.module.ts
+++ backend/src/office/office.module.ts
@@ -1,13 +1,14 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { FilesModule } from '../files/files.module';
 import { PermissionsModule } from '../permissions/permissions.module';
+import { StorageModule } from '../storage/storage.module';
 import { OfficeController } from './office.controller';
 import { OfficeService } from './office.service';
 import { OfficeCollaborationService } from './office-collaboration.service';
 import { OfficeConversionService } from './office-conversion.service';
 
 @Module({
-  imports: [forwardRef(() => FilesModule), PermissionsModule],
+  imports: [forwardRef(() => FilesModule), PermissionsModule, StorageModule],
   controllers: [OfficeController],
   providers: [OfficeService, OfficeConversionService, OfficeCollaborationService],
   exports: [OfficeService],
