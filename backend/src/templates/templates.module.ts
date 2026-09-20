4df8bc546c1ef6c1a86a4a0c7dfb9d108807fd07
osama abu sharekh <abosharekhosama@gmail.com>
2026-09-20 23:32:09 +0300

    fix: resolve nest main bodyparser and office conversion typescript errors

--- backend/src/templates/templates.module.ts
+++ backend/src/templates/templates.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { FilesModule } from '../files/files.module';
 import { StorageModule } from '../storage/storage.module';
 import { OfficeModule } from '../office/office.module';
@@ -7,7 +7,7 @@ import { TemplatesService } from './templates.service';
 import { PublicTemplateSeedService } from './public-template-seed.service';
 
 @Module({
-  imports: [FilesModule, StorageModule, OfficeModule],
+  imports: [forwardRef(() => FilesModule), StorageModule, forwardRef(() => OfficeModule)],
   controllers: [TemplatesController],
   providers: [TemplatesService, PublicTemplateSeedService],
   exports: [TemplatesService],
--- backend/src/workflows/workflows.module.ts
+++ backend/src/workflows/workflows.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { WorkflowsController } from './workflows.controller';
 import { WorkflowsService } from './workflows.service';
 import { WorkflowEngineService } from './workflow-engine.service';
@@ -6,5 +6,5 @@ import { SharesModule } from '../shares/shares.module';
 import { CustomFunctionExecutor } from './custom-function.executor';
 import { ConnectionsModule } from '../connections/connections.module';
 import { TemplatesModule } from '../templates/templates.module';
-@Module({ imports: [SharesModule, ConnectionsModule, TemplatesModule], controllers: [WorkflowsController], providers: [WorkflowsService, WorkflowEngineService, CustomFunctionExecutor], exports: [WorkflowEngineService] })
+@Module({ imports: [SharesModule, ConnectionsModule, forwardRef(() => TemplatesModule)], controllers: [WorkflowsController], providers: [WorkflowsService, WorkflowEngineService, CustomFunctionExecutor], exports: [WorkflowEngineService] })
 export class WorkflowsModule {}
